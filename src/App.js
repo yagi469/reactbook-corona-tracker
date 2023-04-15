9ff126d7bbd6295a0f0577eae18e7619c828098a
yaginumatti <ryohei122yagi@gmail.com>
2023-04-15 17:17:58 +0900

    Titleコンポーネントの作成

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import TopPage from './pages/TopPage';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-
+      <h1>こんばんは</h1>
+      <TopPage />
     </div>
   );
 }
