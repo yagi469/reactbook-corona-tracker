f88e83e0a629a2b298fb13af7316888c43c15ae4
yaginumatti <ryohei122yagi@gmail.com>
2023-04-16 18:24:07 +0900

    Results.jsの修正

--- src/App.js
+++ src/App.js
@@ -5,13 +5,29 @@ import './App.css';
 
 function App() {
   const [country, setCountry] = useState("");
+  const [countryData, setCountryData] = useState({
+    date: "",
+    newConfirmed: "",
+    totalConfirmed: "",
+    newRecovered: "",
+    totalRecovered: "",
+  });
   const getCountryData = () => {
-    fetch(`https://api.covid19api.com/country/${country}`)
+    fetch(`https://monotein-books.vercel.app/api/corona-tracker/country/${country}`)
     .then(res => res.json())
-    .then(data => console.log(data))
+    .then(data => {
+      setCountryData({
+        date: data[data.length - 1].Date,
+        newConfirmed: data[data.length - 1].Confirmed - data[data.length - 2].Confirmed,
+        totalConfirmed: data[data.length - 1].Confirmed,
+        newRecovered: data[data.length - 1].Recovered - data[data.length - 2].Recovered,
+        totalRecovered: data[data.length - 1].Recovered
+      });
+    })
   }
   return (
     <div>
+      {console.log(countryData)}
       <TopPage countriesJson={countriesJson} setCountry={setCountry} getCountryData={getCountryData}/>
     </div>
   );
